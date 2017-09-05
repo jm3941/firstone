7286cb4cb4c04195f44991ee82ac6942d137d886
jm3941 <31648089+jm3941@users.noreply.github.com>
2017-09-05 16:37:49 +0800

    Create create_cities-controller.json.sh

---
+++ create_cities-controller.json.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+cat > cities-controller.json <<EOF
+{
+  "kind": "ReplicationController",
+  "apiVersion": "v1",
+  "metadata": {
+    "name": "cities",
+    "labels": {
+      "name": "cities"
+    }
+  },
+  "spec": {
+    "replicas": 3,
+    "selector": {
+      "name": "cities"
+    },
+    "template": {
+      "metadata": {
+        "labels": {
+          "name": "cities",
+          "deployment": "${WERCKER_GIT_COMMIT}"
+        }
+      },
+      "spec": {
+        "containers": [
+          {
+            "imagePullPolicy": "Always",
+            "image": "quay.io/luhuang/demo:${WERCKER_GIT_COMMIT}",
+            "name": "cities",
+            "ports": [
+              {
+                "name": "http-server",
+                "containerPort": 5000,
+                "protocol": "TCP"
+              }
+            ]
+          }
+        ]
+      }
+    }
+  }
+}
